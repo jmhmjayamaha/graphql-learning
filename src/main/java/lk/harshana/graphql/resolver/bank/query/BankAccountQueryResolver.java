15e5a163c20338e2417e841be389ad996f8d56ef
jmhmjayamaha <jmhmjayamaha@gmail.com>
2021-09-19 18:46:20 +0530

    method selectionSet()

--- src/main/java/lk/harshana/graphql/resolver/bank/query/BankAccountQueryResolver.java
+++ src/main/java/lk/harshana/graphql/resolver/bank/query/BankAccountQueryResolver.java
@@ -1,10 +1,13 @@
 package lk.harshana.graphql.resolver.bank.query;
 
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.SelectedField;
 import lk.harshana.graphql.domain.BankAccount;
 import lk.harshana.graphql.domain.Client;
 import lk.harshana.graphql.domain.Currency;
@@ -14,14 +17,17 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class BankAccountQueryResolver implements GraphQLQueryResolver {
 
-	public BankAccount bankAccount(UUID id) {
+	public BankAccount bankAccount(UUID id, DataFetchingEnvironment environment) {
 		log.info("Bank account information ");
 		
-//		var clientA = Client.builder().id(id).firstName("harshana").lastName("madusanka").build();
-//		var clientB = Client.builder().id(id).firstName("kawshalya").lastName("madushani").build();
-//		
-//		clientA.setClient(clientB);
-//		clientB.setClient(clientA);
+		var requestedFields = environment.getSelectionSet().getFields().stream().map(SelectedField::getName)
+				.collect(Collectors.toSet());
+
+		requestedFields.forEach(name -> log.info(name));
+				
+		if(environment.getSelectionSet().contains("currency")) {
+			log.info("currency is in the filed set");
+		}
 		
 		return BankAccount
 				.builder()
